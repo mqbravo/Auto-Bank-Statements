4e8bb222f1065a5f90b95c83ab7650024a061988
Mauricio Quiros <mqbravo@gmail.com>
2021-09-26 02:39:47 -0600

    Changed to Tabula implementation

--- Bank_Statement_Scrapping.py
+++ Bank_Statement_Scrapping.py
@@ -1,43 +1,14 @@
-import pdfplumber as pdfp
-from datetime import datetime
+import tabula
 import pandas as pd
-from numpy import nan
 
-columns = ["Date", "Ag.Movm", "Doc/Ref", "Concept", "Amount", "Balance"]
-data_clean = []
+dfs = tabula.read_pdf("CU_202131_CU_12011274100.pdf", pages="all")
+del dfs[0]
 
-with pdfp.open("CU_202131_CU_12011274100.pdf") as file:
-    for page in file.pages[1:-1]:
-        content = page.extract_text()
-        content_lines = content.split("\n")
+columns = ["Date", "Ag.Movm", "Concept", "Debit", "Credit", "Balance"]
+dfs = [df.dropna(axis="columns", how="all").set_axis(columns, axis=1) for df in dfs]
 
-        data = [i.split(" ") for i in content_lines[4:-1]]
+statement = pd.DataFrame()
+for df in dfs:
+    statement = statement.append(df)
 
-        for i, line in enumerate(data):
-            try:
-                date = datetime.strptime(line.pop(0), "%d/%m/%Y").date()
-                ag_movm = line.pop(0)
-                doc_ref = line.pop(0)
-                balance = line.pop(-1)
-                amount = line.pop(-1)
-                concept = nan if len(line) == 0 else " ".join(line)
-
-                new_data_line = [date, ag_movm, doc_ref, concept, amount, balance]
-                data_clean.append(new_data_line)
-
-            except:
-                print("Malformed line. Skipping 1")
-                continue
-
-df = pd.DataFrame(data_clean, columns=columns)
-
-is_debit = [True]
-current_balance = df["Balance"][0]
-
-for balance in df["Balance"][1:]:
-    is_debit.append(balance < current_balance)
-    current_balance = balance
-
-df["Is_Debit"] = is_debit
-
-print(len(df))
+statement = statement[:-2]
